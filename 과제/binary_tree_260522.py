678cfbe2d755f043a0d9b36ea026fe8f60833799
DESKTOP-QQTHH1M\USER <radio071218@gmail.com>
2026-05-22 10:11:26 +0900

    no message

--- 과제/binary_tree_260522.py
+++ 과제/binary_tree_260522.py
@@ -1,29 +1,24 @@
 def make_tree(tree):
     mid, left, right = input().split()
-    if left == '.':
-        left = None
-    if right == '.':
-        right = None
     tree[mid] = [left, right]
-    return mid
 
 # 딕셔너리 선위 순회
 def preorder(node):
-    if node == None: return
+    if node == '.': return
     print(node, end = '')
     preorder(tree[node][0])
     preorder(tree[node][1])
 
 # 딕셔너리 중위 순회
 def inorder(node):
-    if node == None: return
+    if node == '.': return
     inorder(tree[node][0])
     print(node, end = '')
     inorder(tree[node][1])
 
 # 딕셔너리 후위 순회
 def postorder(node):
-    if node == None: return
+    if node == '.': return
     postorder(tree[node][0])
     postorder(tree[node][1])
     print(node, end = '')
@@ -32,17 +27,17 @@ def postorder(node):
 count = int(input())
 tree = {}
 for i in range (count):
-    if i != 0:
-        make_tree(tree)
-    else:
-        root = make_tree(tree)
-
+    make_tree(tree)
+    
+root = list(tree.keys())[0]
 
 print("전위 순회한 결과 : ", end = '')
 preorder(root)
 print()
+
 print("중위 순회한 결과 : ", end = '')
 inorder(root)
 print()
+
 print("후위 순회한 결과 : ", end = '')
 postorder(root)
